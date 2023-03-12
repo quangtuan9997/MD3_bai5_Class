902f51f936e46362aaaa1ef1dbf8141da4c7c944
quangtuan9997 <109707621+quangtuan9997@users.noreply.github.com>
2023-03-12 23:52:25 +0700

    Add files via upload

---
+++ class_baiTap/src/stopWatch/StopWatch.java
@@ -0,0 +1,17 @@
+package stopWatch;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+
+    public void start(){
+        startTime = System.currentTimeMillis();
+    }
+    public void end(){
+        endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime(){
+        return endTime - startTime;
+    }
+}
